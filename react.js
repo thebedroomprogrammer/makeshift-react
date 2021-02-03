3757ba0b91dd65df191db1f0e0cfaa2e4a86709e
Arpit Dubey <arpit@gamezop.co>
2021-02-03 19:44:33 +0000

    React's createElement and ReactDOM's render function done. with styles

---
+++ react.js
@@ -0,0 +1,65 @@
+//https://codesandbox.io/s/javascript-forked-7ngvn
+
+const React = {
+  createTextElement(text) {
+    return {
+      type: "TEXT_ELEMENT",
+      props: {
+        nodeValue: text,
+        children: []
+      }
+    };
+  },
+  createElement(type, props, ...children) {
+    return {
+      type,
+      props:{
+        ...props,
+        children: children.map((child) =>
+        typeof child === "object" ? child : React.createTextElement(child)
+      )
+      },
+      
+    };
+  }
+};
+
+const camelToKebabCase = str => str.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
+
+const ReactDOM = {
+  isProperty(key) {
+    return key !== "children" && key!== "style";
+  },
+  render(element, container) {
+    const dom =
+      element.type == "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(element.type);
+   
+      element.props.children.forEach(child =>
+        ReactDOM.render(child, dom)
+      )
+
+    let style= "";
+    if(element.props.style){
+      Object.keys(element.props.style).forEach(property=>{
+        style += `${camelToKebabCase(property)}:${element.props.style[property]};`
+      })
+    }
+
+    if(style){
+      dom["style"] = style;
+    }
+
+    Object.keys(element.props)
+    .filter(ReactDOM.isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    });
+
+    container.appendChild(dom);
+  }
+};
+
+
+ReactDOM.render(React.createElement("h1", { id: "heading",style:{backgroundColor:"red",fontSize:"30px",backgroundImage:"linear-gradient(to right,black,red)"} }, "Hi"),document.getElementById("root"));
