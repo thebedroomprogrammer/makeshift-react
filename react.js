6f628b1825f69aebee5f4ec3f3b8eb91a96a47b1
Arpit Dubey <arpit@gamezop.co>
2021-02-03 19:47:35 +0000

    testing more styles

--- react.js
+++ react.js
@@ -1,4 +1,4 @@
-//https://codesandbox.io/s/javascript-forked-7ngvn
+https://codesandbox.io/s/javascript-forked-7ngvn?file=/index.js
 
 const React = {
   createTextElement(text) {
@@ -13,53 +13,71 @@ const React = {
   createElement(type, props, ...children) {
     return {
       type,
-      props:{
+      props: {
         ...props,
         children: children.map((child) =>
-        typeof child === "object" ? child : React.createTextElement(child)
-      )
-      },
-      
+          typeof child === "object" ? child : React.createTextElement(child)
+        )
+      }
     };
   }
 };
 
-const camelToKebabCase = str => str.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
+const camelToKebabCase = (str) =>
+  str.replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`);
 
 const ReactDOM = {
   isProperty(key) {
-    return key !== "children" && key!== "style";
+    return key !== "children" && key !== "style";
   },
   render(element, container) {
     const dom =
-      element.type == "TEXT_ELEMENT"
+      element.type === "TEXT_ELEMENT"
         ? document.createTextNode("")
         : document.createElement(element.type);
-   
-      element.props.children.forEach(child =>
-        ReactDOM.render(child, dom)
-      )
 
-    let style= "";
-    if(element.props.style){
-      Object.keys(element.props.style).forEach(property=>{
-        style += `${camelToKebabCase(property)}:${element.props.style[property]};`
-      })
+    element.props.children.forEach((child) => ReactDOM.render(child, dom));
+
+    let style = "";
+    if (element.props.style) {
+      Object.keys(element.props.style).forEach((property) => {
+        style += `${camelToKebabCase(property)}:${
+          element.props.style[property]
+        };`;
+      });
     }
 
-    if(style){
+    if (style) {
       dom["style"] = style;
     }
 
     Object.keys(element.props)
-    .filter(ReactDOM.isProperty)
-    .forEach(name => {
-      dom[name] = element.props[name]
-    });
+      .filter(ReactDOM.isProperty)
+      .forEach((name) => {
+        dom[name] = element.props[name];
+      });
 
     container.appendChild(dom);
   }
 };
 
+ReactDOM.render(
+  React.createElement(
+    "div",
+    {
+      id: "heading",
+      style: {
+        backgroundColor: "red",
+        fontSize: "30px",
+        height:"100px",
+        width:"100px",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center"
+      }
+    },
+    "Hi"
+  ),
+  document.getElementById("root")
+);
 
-ReactDOM.render(React.createElement("h1", { id: "heading",style:{backgroundColor:"red",fontSize:"30px",backgroundImage:"linear-gradient(to right,black,red)"} }, "Hi"),document.getElementById("root"));
