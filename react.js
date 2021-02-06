d70b84ee4ef701012e75e311e8356d945dd516fb
Arpit Dubey <arpit@gamezop.co>
2021-02-06 20:59:09 +0000

    reconciliation and fiber updattio

--- react.js
+++ react.js
@@ -1,4 +1,4 @@
-https://codesandbox.io/s/javascript-forked-7ngvn?file=/index.js:0-3825
+//https://codesandbox.io/s/javascript-forked-7ngvn?file=/index.js:0-6306
 
 function createTextElement(text) {
   return {
@@ -24,54 +24,110 @@ function createElement(type, props, ...children) {
 const camelToKebabCase = (str) =>
   str.replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`);
 
+const isEvent = (key) => key.startsWith("on");
+const isProperty = (key) => key !== "children" && !isEvent(key);
+
 function createDom(fiber) {
   const dom =
     fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(fiber.type);
 
-  const isProperty = (key) => key !== "children";
   Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
       dom[name] = fiber.props[name];
     });
 
-  let style = "";
-  if (fiber.props.style) {
-    Object.keys(fiber.props.style).forEach((property) => {
-      style += `${camelToKebabCase(property)}:${fiber.props.style[property]};`;
-    });
-  }
-
-  if (style) {
-    dom["style"] = style;
-  }
+  updateDom(dom, {}, fiber.props);
 
   return dom;
 }
 
 let nextUnitOfWork = null;
 let wipRoot = null;
+let currentRoot = null;
+let deletions = null;
 function render(element, container) {
+  deletions = [];
   wipRoot = {
     dom: container,
     props: {
       children: [element]
-    }
+    },
+    alternate: currentRoot
   };
   nextUnitOfWork = wipRoot;
 }
 
 function commitRoot() {
+  deletions.forEach(commitWork);
   commitWork(wipRoot.child);
+  currentRoot = wipRoot;
   wipRoot = null;
 }
+
+const isNew = (prev, next) => (key) => prev[key] !== next[key];
+const isGone = (prev, next) => (key) => !(key in next);
+
+function updateDom(dom, prevProps, nextProps) {
+  //Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = "";
+    });
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name];
+    });
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+
+  let style = "";
+
+  if (nextProps && nextProps.style) {
+    Object.keys(nextProps.style).forEach((property) => {
+      style += `${camelToKebabCase(property)}:${nextProps.style[property]};`;
+    });
+  }
+
+  if (style) {
+    dom["style"] = style;
+  }
+}
+
 function commitWork(fiber) {
   if (!fiber) {
     return;
   }
   const domParent = fiber.parent.dom;
+  if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  } else if (fiber.effectTag === "DELETION") {
+    domParent.removeChild(fiber.dom);
+  }
   domParent.appendChild(fiber.dom);
   commitWork(fiber.child);
   commitWork(fiber.sibling);
@@ -92,6 +148,56 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop);
 
+function reconcileChildren(wipFiber, elements) {
+  let index = 0;
+  let prevSibling = null;
+
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+
+  while (index < elements.length || oldFiber !== null) {
+    const element = elements[index];
+    let newFiber = null;
+
+    const sameType = oldFiber && element && element.type === oldFiber.type;
+
+    if (sameType) {
+      // TODO update the node
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE"
+      };
+    }
+    if (element && !sameType) {
+      // TODO add this node
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT"
+      };
+    }
+    if (oldFiber && !sameType) {
+      // TODO delete the oldFiber's node
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+}
+
 function performUnitOfWork(fiber) {
   // TODO add dom node
   if (!fiber.dom) {
@@ -104,25 +210,7 @@ function performUnitOfWork(fiber) {
 
   // TODO create new fibers
   const elements = fiber.props.children;
-  let index = 0;
-  let prevSibling = null;
-  while (index < elements.length) {
-    const element = elements[index];
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null
-    };
-
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-    prevSibling = newFiber;
-    index++;
-  }
+  reconcileChildren(fiber, elements);
   // TODO return next unit of work
   if (fiber.child) {
     return fiber.child;
@@ -166,21 +254,21 @@ React.render(
         {
           style: { height: "200px", width: "200px" }
         },
-        "lvl1-child2-child1",
+        "lvl1-child2-child1"
       ),
       React.createElement(
         "div",
         {
           style: { height: "200px", width: "200px" }
         },
-        "lvl1-child2-child3",
+        "lvl1-child2-child3"
       ),
       React.createElement(
         "div",
         {
           style: { height: "200px", width: "200px" }
         },
-        "lvl1-child2-child4",
+        "lvl1-child2-child4"
       )
     ),
     React.createElement(
@@ -193,6 +281,4 @@ React.render(
   ),
   document.getElementById("root")
 );
-
-console.log(wipRoot);
 
